65bcb1010f210e9c80433c9a78724e2edc4de0e9
isaac-newsum-snowflake <110473552+isaac-newsum-snowflake@users.noreply.github.com>
2022-08-02 07:45:25 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import pandas
 import streamlit
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(fruit_list)
 
 streamlit.multiselect("Pick a fruit:", list(fruit_list.index))
 
